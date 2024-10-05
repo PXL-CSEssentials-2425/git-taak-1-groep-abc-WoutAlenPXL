6fcd7c0d8de8dcc6c790fe49c91b745e3554de79
WoutAlenPXL <148558963+WoutAlenPXL@users.noreply.github.com>
2024-10-05 14:32:22 +0200

    KLEUR-05
    Informatie over kleur bij klikken

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,28 +14,34 @@ namespace ColorPsychology
         private void RedButton_Click(object sender, RoutedEventArgs e)
         {
             ColorCodeTextBox.Text = "#FF0000"; // Rood
+            ColorInfoTextBox.Text = "Rood is de kleur van warmte";
         }
 
         // Event handler voor Gele afbeelding
         private void YellowButton_Click(object sender, RoutedEventArgs e)
         {
             ColorCodeTextBox.Text = "#FFFF00"; // Geel
+            ColorInfoTextBox.Text = "Geel is de kleur van levenslust";
         }
 
         // Event handler voor Groene afbeelding
         private void GreenButton_Click(object sender, RoutedEventArgs e)
         {
             ColorCodeTextBox.Text = "#008000"; // Groen
+            ColorInfoTextBox.Text = "Groen is de kleur van genezing";
         }
 
         // Event handler voor Blauwe afbeelding
         private void BlueButton_Click(object sender, RoutedEventArgs e)
         {
             ColorCodeTextBox.Text = "#0000FF"; // Blauw
+            ColorInfoTextBox.Text = "Blauw is de kleur van intelligentie";
         }
     }
 
 }
+
+
     
 
 
